1a9fbff773d3f4ef48243a44e97acae89e077994
Wachidota <jmolascoaga2@gmail.com>
2026-04-30 17:57:04 -0300

    deploycompatibilidad

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 
-import cors from "cors";
 import { connectDB } from "./v1/config/db.js";
 import v1Router from "./v1/v1.routes.js";
 import { notFoundMiddleware } from "./v1/middlewares/notFound.middleware.js";
 import { errorMiddleware } from "./v1/middlewares/error.middleware.js";
 
-
 dotenv.config();
-connectDB();
 
 const app = express();
 
-app.use(cors());
+// 🔌 Conexión DB (segura para serverless)
+if (process.env.MONGODB_URI) {
+    connectDB();
+} else {
+    console.error("❌ MONGODB_URI no definido");
+}
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.json({mensaje: "API funcionando"});
+    res.json({ mensaje: "API funcionando" });
 });
 
 app.use("/v1", v1Router);
 
 app.use(notFoundMiddleware);
-
 app.use(errorMiddleware);
 
 export default app;
